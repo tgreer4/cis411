3472747488df300c063acde20ece2a59ff4e0534
tgreer4 <95147728+tgreer4@users.noreply.github.com>
2022-11-02 00:14:52 -0400

    viterbi function

--- Assignment2/baseline.py
+++ Assignment2/baseline.py
@@ -5,21 +5,28 @@ import sys
 
 tag_freq = {}
 
-def tag_max():
+def tag_max(dictionary):
+    for key, value in dictionary.items():
+        if(len(value) > 1):
+            print(key, '->', value)
     return 0
 
 def dict_vals(dict_line):
-    temp_dict = {}
+    temp_list = []
     for a in range(len(dict_line)):
-        temp_line = dict_line[a].split("/") # assume that / is always in line
-        temp_dict[temp_line[0].lower()] = {temp_line[1]: 1}
-        if temp_dict[temp_line[0].lower()][temp_line[1]] in tag_freq:
-            tag_freq[temp_line[0].lower()][temp_line[1]] += 1
+        temp_line = dict_line[a].split("/") # assume that / is always in line [word, tag]
+
+        if temp_line[0] in tag_freq:
+            if tag_freq[temp_line[0]]:
+                tag_freq[temp_line[0]][1] += 1
+            else:
+
         else:
-            tag_freq[temp_line[0].lower()] = {temp_line[1]: 1}
+            tag_freq[temp_line[0]] = [temp_line[1], 1]
+        temp_list.clear()
 
 def preprocess(line):
-    unwanted = ('!', '?', '\%', ',', '(', ')', '&', '$')
+    unwanted = ('!', '?', '\%', ',', '(', ')', '&', '$', '')
     for a in range(len(line)):
         for u in unwanted: line[a] = line[a].replace(u, '')
         back_slash = re.findall(r'\\/', line[a])
@@ -40,7 +47,7 @@ while len(sys.argv) > 1:  # so long as arguments include files to open
             line = line.split()
             line = preprocess(line)
             dict_vals(line)
-            tag_max()
-    print(tag_freq)
+            tag_max(tag_freq)
+    # print(tag_freq)
 
     break
