af32d52cf18260de71f3d2f28143fa96a03e66ac
tgreer4 <95147728+tgreer4@users.noreply.github.com>
2022-11-12 20:15:10 -0500

    fixed populate_folds, works now

--- Assignment 3/WSD.py
+++ Assignment 3/WSD.py
@@ -1,31 +1,36 @@
 import sys
 import math
 import random
-import re
 
-senses = {1:{'name': ' ', 'total': 0}, 2: {'name': ' ', 'total': 0}}
-folds = {1:[], 2: [], 3: [], 4: [], 5: []}
+senses = {1: {'name': ' ', 'total': 0}, 2: {'name': ' ', 'total': 0}}
+true_sense = {} #id:actual sense
 count_instance = 0
+folds = {1: {}, 2: {}, 3: {}, 4: {}, 5:{}}
 
-stop_words = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
+stop_words = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out',
+              'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into',
+              'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
+              'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
+              'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
+              'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and',
+              'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
+              'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too',
+              'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my',
+              'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
 
-unwanted = ['</context>', '</instance>', '<context>']
+unwanted = ('</context>', '</instance>', '<context>', '<instance id=')
 
+def preprocess(list):
+    res = []
+    for a in list: res.append(a.replace('\n', ''))
 
-# for iterator in range(3, len(text), 7): count_instance += 1
+    while ('' in res): res.remove('')
 
-# def preprocess(textInput):
+    for a in res[:]:
+        if a.startswith(unwanted): res.remove(a)
+    return (res)
 
-    # pattern = '\n'
-    # for line in textInput:
-    #     line = re.sub(pattern, '', line)
-    #     print("line after sub:",line)
-    #     if line in unwanted:
-    #         line = line.replace(line, '')
-
-    # print("textInput: ",textInput)
-
-
+# fix
 def word_bags(fold_list):
     global senses
 
@@ -33,75 +38,69 @@ def word_bags(fold_list):
         begin_index = lines.find('%')
         if begin_index > 0:
             senseid = lines[begin_index:-1]
-            if senses[1]['name'] == ' ':
-                senses[1]['name'] = senseid
-                senses[1]['name']['total'] += 1
-
-            elif senses[1]['name'] == senseid: senses[1]['name']['total'] += 1
-            elif senses[2]['name'] == ' ':
-                senses[2]['name'] = senseid
-                senses[2]['name']['total'] += 1
-            elif senses[2]['name'] == senseid:
-                senses[2]['name']['total'] += 1
+            if senseid in senses.values():
+                if senses[1]['name'] == senseid: senses[1]['name']['total'] += 1
+                elif senses[2]['name'] == senseid: senses[2]['name']['total'] += 1
+            # elif not any(senses.values()):
 
         temp = lines.split()
+        for word in temp:
+            if word in stop_words:
+                temp.remove(word)
 
-
+#fix
 def wsd():
     global folds
-    test_vals = [] #use a random num generator and pop it into a list of folds already used for testing
+    test_vals = []  # use a random num generator and pop it into a list of folds already used for testing
     for a in range(5):
         test_fold = random.randrange(1, 5)
-        while test_fold in test_vals and len(test_vals) < 5: #so long as random # isn't already in tested folds and all folds haven't been tested
+        while test_fold in test_vals and len(
+                test_vals) < 5:  # so long as random # isn't already in tested folds and all folds haven't been tested
             test_fold = random.randrange(1, 5)
         test_vals.pop(test_fold)
 
-        if a == test_fold: continue
-        else: word_bags(folds[a])
+        if a == test_fold:
+            continue
+        else:
+            word_bags(folds[a])
 
-
-def populate_folds(fold_size,text):
+def populate_folds(fold_size, text):
     global folds
-    size_tracker = fold_size
-    temp_list = []
+
+    size_tracker = fold_size *2
     previous_index = 0
+    instance_val = ''
+    list = []
 
-    for key in range(len(folds)):
+    for key in range(1,len(folds)+1):
         for iterator in range(previous_index, len(text)):
+
+            instance_index = text[iterator].find('.')
+            if instance_index > -1 and text[iterator].startswith('<'):
+                instance_val = text[iterator][instance_index+1:instance_index+8]
+
+            correct_sense_i = text[iterator].find('%')
+            if correct_sense_i > -1:
+                folds[key][instance_val] = [(text[iterator][correct_sense_i + 1:-3])]
+                true_sense[instance_val] = text[iterator][correct_sense_i + 1:-3]
+
+            if iterator+1<len(text) and not text[iterator+1].startswith('<'):
+                folds[key][instance_val].append(text[iterator+1])
+
+            size_tracker -= 1  # make sure number of instances within a fold are correct
+            list.clear()
             if size_tracker == 0:
-                previous_index = iterator #keep index for next fold
+                previous_index = iterator+1 # keep index for next fold
                 break
-
-            # fix
-            instance_index = text[iterator].find('.') #keeping instance id to use later on
-            if instance_index > -1:
-                split_text = text[iterator].split()
-                instance_index = split_text[0].find('.')
-                temp_list.append(split_text[0][instance_index:-1])
-
-            #fix
-            instance_sentence = iterator.find('<') #obtaining
-            if instance_sentence > -1: temp_list.append(text[iterator])
-
-            correct_sense_i = iterator.find('%')
-            if correct_sense_i > -1: temp_list.append(text[iterator][correct_sense_i:-3])
-
-            folds[key].pop(temp_list)
-
-            size_tracker -= 1 #make sure number of instances within a fold are correct
-
-            if (len(text)-iterator) < fold_size:
-                size_tracker = len(text)-iterator
+        size_tracker=fold_size*2
 
 
 while len(sys.argv) > 1:  # so long as arguments include files to open, assume only 1
     file = open(sys.argv[1])
     text = file.readlines()
-    # preprocess(text)
-    # text.remove('<context>')
-    print("text: ",text)
-    break
 
-    #
-    # fold_size = math.ceil(count_instance / 5)
-    # populate_folds(fold_size, text)
+    text = preprocess(text)
+    fold_size = math.ceil((len(text)/2) / 5)
+    populate_folds(fold_size, text)
+
+    break
